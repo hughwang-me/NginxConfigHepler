cae3aef38ee61d360f684210eada77c7c2bae804
wanghuan <hunter.v.wang@gmail.com>
2017-07-12 16:14:49 +0800

    update by push.sh

--- qc.keruis.com/deploy.sh
+++ qc.keruis.com/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd=`pwd`
-echo $pwd
+echo $0
 cp /usr/local/nginx/conf/nginx.conf nginx.conf.old
 mv $pwd/nginx.conf /usr/local/nginx/conf/nginx.conf
 /usr/local/nginx/sbin/nginx -s reload
