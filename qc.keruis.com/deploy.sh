9907eddea39153a2bb4c15daa3b88682a67c843e
wanghuan <hunter.v.wang@gmail.com>
2017-07-12 17:02:12 +0800

    update by push.sh

--- qc.keruis.com/deploy.sh
+++ qc.keruis.com/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 basepath=$(cd `dirname $0`; pwd)
 cp /usr/local/nginx/conf/nginx.conf nginx.conf.old
-cp $pwd/$0 /usr/local/nginx/conf/nginx.conf
+cp $basepath/nginx.conf /usr/local/nginx/conf/nginx.conf
 /usr/local/nginx/sbin/nginx -s reload
