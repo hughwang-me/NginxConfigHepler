5cb18ff3c1ce51f62b56872d39d6e9a244839432
wanghuan <hunter.v.wang@gmail.com>
2017-07-12 16:06:56 +0800

    update by push.sh

--- qc.keruis.com/deploy.sh
+++ qc.keruis.com/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp /usr/local/nginx/conf/nginx.conf nginx.conf.old
 mv nginx.conf /usr/local/nginx/conf/nginx.conf
-/usr/local/nginx/sbin/nginx -g HUP
+/usr/local/nginx/sbin/nginx -s reload
